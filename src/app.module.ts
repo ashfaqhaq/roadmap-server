9426167854b0b70307178dccf53a6100814c61bc
ashfaqhaq <ashfaq.28010@gmail.com>
2021-08-27 08:28:38 +0530

    github actions and main branch

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
+import { RoadmapController } from './roadmap/roadmap.controller';
+import { RoadmapService } from './roadmap/roadmap.service';
+import { RoadmapModule } from './roadmap/roadmap.module';
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), RoadmapModule],
+  controllers: [AppController, RoadmapController],
+  providers: [AppService, RoadmapService],
+})
+export class AppModule {}
