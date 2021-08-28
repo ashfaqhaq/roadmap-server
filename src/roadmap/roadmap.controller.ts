b52c663f28a4f7f6e1b560a9b6b0aabe402a10be
ashfaqhaq <ashfaq.28010@gmail.com>
2021-08-28 10:21:42 +0530

    CRUD operations and updatedDtos

--- src/roadmap/roadmap.controller.ts
+++ src/roadmap/roadmap.controller.ts
@@ -1,19 +1,40 @@
-import { Controller,Get ,Post,Body } from '@nestjs/common';
+import { Controller, Get, Post, Body,Delete } from '@nestjs/common';
 import { RoadmapService } from './roadmap.service'
-import { RoadmapDto } from './roadmap.dto';
+import { RoadmapDto,UpdateRoadmapDto } from './roadmap.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Put,Param } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
+@ApiTags("Roadmaps")
 @Controller('roadmap')
 export class RoadmapController {
-    constructor(private readonly roadmapService: RoadmapService) {}
+    constructor(private readonly roadmapService: RoadmapService) { }
     @Get()
     findAll(): Promise<RoadmapDto[]> {
         return this.roadmapService.findAll();
     }
 
-    @Post()
-    async create(@Body() roadmapDto: RoadmapDto){
-        return this.roadmapService.createRoadmap(roadmapDto)
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        console.log(id);
+        return this.roadmapService.findOne(id);
     }
+
+    @Post()
+    async create(@Body() roadmapDto: RoadmapDto) {
+        return this.roadmapService.create(roadmapDto)
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateRoadmapDto: UpdateRoadmapDto) {
+      return this.roadmapService.update(id,updateRoadmapDto);
+    }
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+      return this.roadmapService.delete(id);
+    }
+   
+
 }
 
 
--- src/roadmap/roadmap.dto.ts
+++ src/roadmap/roadmap.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty,IntersectionType } from '@nestjs/swagger';
-
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class MetadataDTO {
     @ApiProperty()
     readonly name: string;
@@ -13,7 +13,17 @@ export class StepDto {
     @ApiProperty()
     readonly steps: any;
 }
+
 export class RoadmapDto extends IntersectionType(
     MetadataDTO,
     StepDto,
   ) {}
+
+  export class UpdateRoadmapDto {
+    @ApiProperty()
+    readonly name?: string;
+    @ApiProperty()
+    readonly description?: string;
+    @ApiProperty()
+    readonly rating?: number;
+}
