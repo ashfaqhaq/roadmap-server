2c6d64123755da13b4170196a23d6d97c9318943
ashfaqhaq <ashfaq.28010@gmail.com>
2021-08-28 23:44:52 +0530

    unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RoadmapController } from './roadmap/roadmap.controller';
-import { RoadmapService } from './roadmap/roadmap.service';
 import { RoadmapModule } from './roadmap/roadmap.module';
 import { ConfigModule } from '@nestjs/config';
 
-import {MONGO_URI} from '../config.js';
+
 
 @Module({
   imports: [ConfigModule.forRoot(),MongooseModule.forRoot(process.env.MONGO_URI), RoadmapModule],
