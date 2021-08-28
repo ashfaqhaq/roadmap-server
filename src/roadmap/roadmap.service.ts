df2ecc3fea42de7ce4b48311d32bebdc110a289d
ashfaqhaq <ashfaq.28010@gmail.com>
2021-08-28 10:22:02 +0530

    validation pipes

--- src/roadmap/roadmap.service.ts
+++ src/roadmap/roadmap.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose'
 import { InjectModel } from '@nestjs/mongoose';
 import { IRoadmap } from './interfaces/roadmap.interface';
-import { RoadmapDto } from './roadmap.dto';
+import { RoadmapDto, UpdateRoadmapDto } from './roadmap.dto';
 import { HttpException } from '@nestjs/common';
 
 
@@ -11,7 +11,7 @@ export class RoadmapService {
 
   constructor(@InjectModel('Roadmap') private readonly roadmapModel: Model<IRoadmap>) { }
 
-  public async findAll():Promise<RoadmapDto[]> {
+  public async findAll(): Promise<RoadmapDto[]> {
     const roadmap = await this.roadmapModel.find().exec();
     if (!roadmap) {
       throw new HttpException('Not found', 404);
@@ -19,10 +19,27 @@ export class RoadmapService {
     return roadmap;
   }
 
-  
-  public async createRoadmap(newRoadmap:RoadmapDto) {
+  public async findOne(id: string) {
+    const roadmap = await this.roadmapModel.findById(id);
+    if (!roadmap) {
+      throw new HttpException('Not found', 404);
+    }
+    return roadmap;
+  }
+
+  public async create(newRoadmap: RoadmapDto) {
     const roadmap = await new this.roadmapModel(newRoadmap);
     return roadmap.save()
   }
+
+  public async update(id: string, newRoadmap: UpdateRoadmapDto) {
+    const roadmap = await new this.roadmapModel(newRoadmap);
+    return await this.roadmapModel.findByIdAndUpdate(id, newRoadmap, { new: true });
+
+  }
+  public async delete(id: string) {
+    return await this.roadmapModel.findByIdAndRemove(id);
+  }
+
 
 }
