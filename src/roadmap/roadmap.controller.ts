5ca09f6de9410426b4b1b21887985bc139a3952c
ashfaqhaq <ashfaq.28010@gmail.com>
2021-09-01 02:13:04 +0530

    enable jwt for endpoints

--- src/roadmap/roadmap.controller.ts
+++ src/roadmap/roadmap.controller.ts
@@ -1,39 +1,47 @@
-import { Controller, Get, Post, Body,Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, UseGuards } from '@nestjs/common';
 import { RoadmapService } from './roadmap.service'
-import { RoadmapDto,UpdateRoadmapDto } from './roadmap.dto';
-import { ApiTags } from '@nestjs/swagger';
-import { Put,Param } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { RoadmapDto, UpdateRoadmapDto } from './roadmap.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Put, Param } from '@nestjs/common';
+import { AuthorizationGuard } from 'src/authorization/authorization.guard';
 
 @ApiTags("Roadmaps")
 @Controller('roadmap')
 export class RoadmapController {
-    constructor(private readonly roadmapService: RoadmapService) { }
-    @Get()
-    findAll(): Promise<RoadmapDto[]> {
-        return this.roadmapService.findAll();
-    }
+  constructor(private readonly roadmapService: RoadmapService) { }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        console.log(id);
-        return this.roadmapService.findOne(id);
-    }
 
-    @Post()
-    async create(@Body() roadmapDto: RoadmapDto) {
-        return this.roadmapService.create(roadmapDto)
-    }
+  @Get()
+  findAll(): Promise<RoadmapDto[]> {
+    return this.roadmapService.findAll();
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() updateRoadmapDto: UpdateRoadmapDto) {
-      return this.roadmapService.update(id,updateRoadmapDto);
-    }
-    @Delete(':id')
-    delete(@Param('id') id: string) {
-      return this.roadmapService.delete(id);
-    }
-   
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    console.log(id);
+    return this.roadmapService.findOne(id);
+  }
+  @UseGuards(AuthorizationGuard)
+  @ApiBearerAuth('token')
+  @Post()
+  async create(@Body() roadmapDto: RoadmapDto) {
+    return this.roadmapService.create(roadmapDto)
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @ApiBearerAuth('token')
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateRoadmapDto: UpdateRoadmapDto) {
+    return this.roadmapService.update(id, updateRoadmapDto);
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @ApiBearerAuth('token')
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.roadmapService.delete(id);
+  }
+
 
 }
 
--- src/roadmap/roadmap.module.ts
+++ src/roadmap/roadmap.module.ts
@@ -3,7 +3,9 @@ import { RoadmapController } from './roadmap.controller';
 import { RoadmapService } from './roadmap.service';
 import { MongooseModule } from '@nestjs/mongoose'
 import { RoadmapSchema } from './schema/roadmap.schema';
-
+import { AuthorizationGuard } from 'src/authorization/authorization.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthorizationModule } from 'src/authorization/authorization.module';
 
 @Module({
     exports: [RoadmapService],
@@ -13,8 +15,11 @@ import { RoadmapSchema } from './schema/roadmap.schema';
                 name: 'Roadmap',
                 schema: RoadmapSchema
             },
-    ]),
-],
+
+        ]),
+        ConfigModule.forRoot()
+
+    ],
     controllers: [RoadmapController,],
     providers: [RoadmapService],
 })
