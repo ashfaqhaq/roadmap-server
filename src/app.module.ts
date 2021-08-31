8fbaec6c83a685ce983cca14d459a5c0c59650e3
ashfaqhaq <ashfaq.28010@gmail.com>
2021-09-01 02:11:34 +0530

    enable JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RoadmapModule } from './roadmap/roadmap.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthorizationModule } from './authorization/authorization.module';
 
 
 
 @Module({
-  imports: [ConfigModule.forRoot(),MongooseModule.forRoot(process.env.MONGO_URI), RoadmapModule],
+  imports: [ConfigModule.forRoot(),MongooseModule.forRoot(process.env.MONGO_URI), RoadmapModule,AuthorizationModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,27 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
   .setTitle('Roadmaps')
   .setDescription('The Roadmaps API description')
   .setVersion('1.0')
   .addTag('Roadmaps')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'token'
+  )
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
 app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
